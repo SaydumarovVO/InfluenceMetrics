74febb31176031e315d7ef98b2d8f3a710202270
Valery_Saydumarov <saidumarov@phystech.edu>
2016-11-01 04:29:04 +0300

    The distance method has been rewritten, it now uses HashMap interface instead of SparseRealMatrix. But it seems, that the same problem is still exists: OutOfRangeException is calling when the program is running, but there was no clear documentation about this error in internet, so it's hard to say exactly what kind of mistake do we have here.

--- src/main/java/ru/saydumarov/Normalization.java
+++ src/main/java/ru/saydumarov/Normalization.java
@@ -3,6 +3,8 @@ package ru.saydumarov;
 import org.apache.commons.math3.linear.*;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Normalization{
     public static double[][] normalize (double[][] data){
@@ -37,20 +39,28 @@ public class Normalization{
 
 
     public static double[][] distance(double[][] data){
+        Map<Double, SparseRealVector> mapData = new HashMap<Double, SparseRealVector>();
         int rows = getAmountOfUsers(data);
-        SparseRealMatrix matrixData = new OpenMapRealMatrix(rows, rows);
-
-        for (int i = 0; i < rows; i++){
-            matrixData.setEntry((int)data[i][0], (int)data[i][1], data[i][2]);
+        SparseRealVector v = new OpenMapRealVector(rows);
+        for (int i = 0; i < data.length; i++){
+            if (mapData.get(new Double(data[i][0])) != null){
+                v = mapData.get(new Double(data[i][0]));
+                v.setEntry((int)data[i][1], data[i][2]);
+                mapData.put(new Double(data[i][0]), v);
+            }
+            else {
+                v.setEntry((int)data[i][1], data[i][2]);
+                mapData.put(new Double(data[i][0]), v);
+            }
         }
 
-        double min = matrixData.getRowVector(0).getL1Distance(matrixData.getRowVector(1));
-        double max = matrixData.getRowVector(0).getL1Distance(matrixData.getRowVector(1));
+        double min = mapData.get(0).getL1Distance(mapData.get(1));
+        double max = mapData.get(0).getL1Distance(mapData.get(1));
 
         for (int i = 0; i < rows - 1; i++){
             for (int j = rows - 1; j > i; j--){
-                SparseRealVector vector1 = (SparseRealVector) matrixData.getRowVector(i);
-                SparseRealVector vector2 = (SparseRealVector) matrixData.getRowVector(j);
+                SparseRealVector vector1 = mapData.get(i);
+                SparseRealVector vector2 = mapData.get(j);
                 if (vector1.getL1Distance(vector2) >= max){
                     max = vector1.getL1Distance(vector2);
                 }
@@ -59,6 +69,7 @@ public class Normalization{
                 }
             }
         }
+
         System.out.println("Максимум: " + max + "; Минимум: " + min);
         double range = max - min;
         double[][] a = new double[1000][2];
@@ -71,8 +82,8 @@ public class Normalization{
         int iterator = 0;
         for (int i = 0; i < rows; i++){
             for (int j = rows - 1; j > i; j--){
-                SparseRealVector vector1 = (SparseRealVector) matrixData.getRowVector(i);
-                SparseRealVector vector2 = (SparseRealVector) matrixData.getRowVector(j);
+                SparseRealVector vector1 = mapData.get(i);
+                SparseRealVector vector2 = mapData.get(j);
                 iterator = (int)Math.floor((vector1.getL1Distance(vector2) - min)/(range/1000));
                 a[iterator][1]++;
             }
@@ -81,9 +92,54 @@ public class Normalization{
         return a;
     }
 
+//    public static double[][] distance(double[][] data){
+//        int rows = getAmountOfUsers(data);
+//        SparseRealMatrix matrixData = new OpenMapRealMatrix(rows, rows);
+//
+//        for (int i = 0; i < rows; i++){
+//            matrixData.setEntry((int)data[i][0], (int)data[i][1], data[i][2]);
+//        }
+//
+//        double min = matrixData.getRowVector(0).getL1Distance(matrixData.getRowVector(1));
+//        double max = matrixData.getRowVector(0).getL1Distance(matrixData.getRowVector(1));
+//
+//        for (int i = 0; i < rows - 1; i++){
+//            for (int j = rows - 1; j > i; j--){
+//                SparseRealVector vector1 = (SparseRealVector) matrixData.getRowVector(i);
+//                SparseRealVector vector2 = (SparseRealVector) matrixData.getRowVector(j);
+//                if (vector1.getL1Distance(vector2) >= max){
+//                    max = vector1.getL1Distance(vector2);
+//                }
+//                if (vector1.getL1Distance(vector2) <= min){
+//                    min = vector1.getL1Distance(vector2);
+//                }
+//            }
+//        }
+//        System.out.println("Максимум: " + max + "; Минимум: " + min);
+//        double range = max - min;
+//        double[][] a = new double[1000][2];
+//
+//        for (int i = 0; i < 1000; i++){
+//            a[i][0] = i * (range/1000) + min;
+//            a[i][1] = 0;
+//        }
+//
+//        int iterator = 0;
+//        for (int i = 0; i < rows; i++){
+//            for (int j = rows - 1; j > i; j--){
+//                SparseRealVector vector1 = (SparseRealVector) matrixData.getRowVector(i);
+//                SparseRealVector vector2 = (SparseRealVector) matrixData.getRowVector(j);
+//                iterator = (int)Math.floor((vector1.getL1Distance(vector2) - min)/(range/1000));
+//                a[iterator][1]++;
+//            }
+//        }
+//
+//        return a;
+//    }
+
     public static void main(String[] args){
         String fileNameOut = "D:\\Progay\\IDEA Workspace\\Influence Metrics\\output.txt";
-        String fileNameIn = "D:\\Progay\\IDEA Workspace\\Influence Metrics\\actions_influence.txt";
+        String fileNameIn = "D:\\Progay\\IDEA Workspace\\Influence Metrics\\input.txt";
         double[][] array = new double[0][];
         try {
             array = FileWorker.read(fileNameIn);
